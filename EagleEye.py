efec643c0de4243c5c7547192dd639fc8ecb5f95
Hydra7 <33226087+Hydra7@users.noreply.github.com>
2017-11-14 01:09:10 +0800

    Update EagleEye.py

--- EagleEye.py
+++ EagleEye.py
@@ -28,13 +28,13 @@ br.addheaders = [
     ('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 
-print " _____            _      _____           "
-print "| ____|__ _  __ _| | ___| ____|   _  ___ "
-print "|  _| / _` |/ _` | |/ _ \  _|| | | |/ _ |"
-print "| |__| (_| | (_| | |  __/ |__| |_| |  __/"
-print "|_____\__,_|\__, |_|\___|_____\__, |\___|"
-print "            |___/             |___/      "
-print "------------Dark-IT Security-------------"
+print(" _____            _      _____           ")
+print("| ____|__ _  __ _| | ___| ____|   _  ___ ")
+print("|  _| / _` |/ _` | |/ _ \  _|| | | |/ _ |")
+print("| |__| (_| | (_| | |  __/ |__| |_| |  __/")
+print("|_____\__,_|\__, |_|\___|_____\__, |\___|")
+print("            |___/             |___/      ")
+print("------------Dark-IT Security-------------")
 target = raw_input('\033[1;m Set Target: ')
 if 'http' in target:
     parsed_uri = urlparse(target)
